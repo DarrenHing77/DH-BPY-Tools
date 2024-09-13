f3ef4c8ad33c05f3544626428ee1f54ad12e7810
Darren Hing <darren.hing77@gmail.com>
2024-09-13 18:57:57 +1000

    added project manager UI

--- DH_Toolkit/menus/main_menu.py
+++ DH_Toolkit/menus/main_menu.py
@@ -4,6 +4,7 @@ from .mesh_menu import draw_mesh, draw_mask
 from ..icons.icons import load_icons
 from .brush_panel import draw_sculpt_panels
 from ..operators.open_proj_dir import DH_OP_Open_Proj_Dir
+from ..operators.project_manager import DH_OP_Proj_Manage, DH_OP_Project_Manager_Popup, DH_OP_CreateProjectDirectories
 
 
 class DH_MT_Main_Menu(bpy.types.Menu):
@@ -12,35 +13,38 @@ class DH_MT_Main_Menu(bpy.types.Menu):
 
     def draw(self, context):
         pie = self.layout.menu_pie()
-        row = pie.row()
-
-        # Create the left column
-        col_left = row.column()
+        
+        # Left column
+        col_left = pie.column()
         box = col_left.box()
         icons = load_icons()
         draw_mesh(box, context)
         col_left.separator()
         draw_mask(box, context)
 
-        # Create the right column
-        col_right = row.column()
-        
+        # Right column
+        col_right = pie.column()
         box = col_right.box()
-        col_right.scale_y = 1.5  # Adjust the size of the column
-        row = pie.row()
-
-        # Create the left column
-        col_left = row.column()
-        box = col_left.box()
-        
+        col_right.scale_y = 1.5  
         draw_sculpt_panels(box, context)
-        row = pie.row()
         
-        pie.operator("dh.open_proj_dir", text="Open Project",icon='FILE_FOLDER')
+        
+        pie.operator("dh.open_proj_dir", text="Open Project", icon='FILE_FOLDER')
 
-        # Create the left column
-        #col_left = row.column()
-        #box = col_left.box()
+        # Add buttons directly to the pie menu
+        col_center = pie.column()
+        col_center.alignment = 'CENTER' # Align items in this column to the center
+
+        # Add "Open Project" to the middle
+        
+
+        # Add an empty space to push "Project Manager" to the bottom
+        col_center.separator()
+        col_center.separator()
+
+        # Add "Project Manager" to the bottom center
+        col_center.operator("dh.create_project_directories", text="Project Manager", icon='FILE_FOLDER')
+       
         
 
         
--- DH_Toolkit/operators/__init__.py
+++ DH_Toolkit/operators/__init__.py
@@ -7,9 +7,11 @@ from .open_proj_dir import DH_OP_Open_Proj_Dir
 from .DCC_Export import DH_OP_dcc_export
 from .mask_tools import DH_OP_MaskExtract
 from .export_obj_multi import DH_OP_export_obj_multi
+from .project_manager import DH_OP_CreateProjectDirectories, DH_OP_Proj_Manage, DH_OP_Project_Manager_Popup
 
 classes = (DH_OP_dcc_import, DH_OP_Decimate,SetDiffuseColorOperator,DH_OP_Open_Proj_Dir,
-           DH_OP_dcc_export, DH_OP_MaskExtract, DH_OP_export_obj_multi
+           DH_OP_dcc_export, DH_OP_MaskExtract, DH_OP_export_obj_multi,
+           DH_OP_CreateProjectDirectories, DH_OP_Proj_Manage, DH_OP_Project_Manager_Popup
            )
 
 def register_operators():
