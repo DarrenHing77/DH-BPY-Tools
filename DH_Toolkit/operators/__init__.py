1a186f0d7b772b7e00cb37fde17a14663fce4925
Darren Hing <darren.hing77@gmail.com>
2024-09-13 11:58:26 +1000

    renamed from operator to operators to avoid stdlib conflicts

---
+++ DH_Toolkit/operators/__init__.py
@@ -0,0 +1,24 @@
+import bpy
+
+from .DCC_Import import DH_OP_dcc_import
+from .simple_decimate import DH_OP_Decimate
+from .color_picker import SetDiffuseColorOperator
+from .open_proj_dir import DH_OP_Open_Proj_Dir
+from .DCC_Export import DH_OP_dcc_export
+from .mask_tools import DH_OP_MaskExtract
+from .export_obj_multi import DH_OP_export_obj_multi
+
+classes = (DH_OP_dcc_import, DH_OP_Decimate,SetDiffuseColorOperator,DH_OP_Open_Proj_Dir,
+           DH_OP_dcc_export, DH_OP_MaskExtract, DH_OP_export_obj_multi
+           )
+
+def register_operators():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+
+def unregister_operators():
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
+        
---
+++ DH_Toolkit/operators/export_obj_multi.py
@@ -0,0 +1,69 @@
+import bpy
+import os
+from bpy.app.handlers import persistent
+
+
+class DH_OP_export_obj_multi(bpy.types.Operator):
+
+    bl_idname = "dh.export_obj_multi"
+    bl_label = "Export_OBJ_Multi"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    
+    def draw(self,context):
+        
+        layout =  self.layout
+       # layout.operator_context = 'INVOKE DEFAULT'
+        layout.label(text ="Multi OBJ Expport")
+        
+        layout.operator(DH_OP_export_obj_multi.bl_idname, text="Dialog Operator")
+    
+    def execute(self, context):
+        # Get the path of the current blend file
+        filepath = bpy.data.filepath
+
+        # Get the directory path
+        directory = os.path.dirname(filepath)
+
+        # Go up a folder
+        export_directory = os.path.abspath(os.path.join(directory, os.pardir))
+
+        # Create the export directory if it doesn't exist
+        export_directory = os.path.join(export_directory, "obj")
+        os.makedirs(export_directory, exist_ok=True)
+
+        # Get the currently selected objects
+        selected_objects = bpy.context.selected_objects
+
+        # Iterate through selected objects and export them individually
+        for obj in selected_objects:
+            bpy.context.view_layer.objects.active = obj
+            obj.select_set(True)
+
+            # Get the object name
+            obj_name = obj.name
+
+            # Check if "_v001" is already in the name
+            if "_v001" not in obj_name:
+                obj_name += "_v001"
+
+            # Set shading to smooth
+            bpy.ops.object.shade_smooth()
+
+            # Set the export path and filename
+            export_filename = os.path.join(export_directory, f"{obj_name}.obj")
+
+            # Export the object as an OBJ file
+            bpy.ops.export_scene.fbx(
+                filepath=export_filename,
+                use_selection=True,
+                
+            )
+
+            obj.select_set(False)
+
+        # Deselect all objects after exporting
+        bpy.ops.object.select_all(action='DESELECT')
+
+
+        return {'FINISHED'}
