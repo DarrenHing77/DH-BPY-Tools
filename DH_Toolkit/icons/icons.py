3d38c270e664d132f243d63cf6f29e40694eaef4
DarrenHing77 <42031409+DarrenHing77@users.noreply.github.com>
2025-06-25 14:24:53 +1000

    updated icons loading logic
    simplified icon loading

--- DH_Toolkit/icons/icons.py
+++ DH_Toolkit/icons/icons.py
@@ -1,52 +1,32 @@
-import inspect
-import os
 import bpy
-import bpy.utils.previews
+import os
 
-
-
-dh_tools_icon_collections = {}
-dh_tools_icons_loaded = False
-
-
-icons_path = os.path.join(os.path.dirname(__file__))
-icons_dir = icons_path.replace("\\","/")
+_icon_collection = None
 
 def load_icons():
-    global dh_tools_icon_collections
-    global dh_tools_icons_loaded
-
-    if dh_tools_icons_loaded: return dh_tools_icon_collections["main"]
-
-    custom_icons = bpy.utils.previews.new()
-
+    global _icon_collection
+    if _icon_collection is None:
+        _icon_collection = bpy.utils.previews.new()
+        icons_dir = os.path.join(os.path.dirname(__file__))
+        
+        # Load all your icons
+        _icon_collection.load("icon_separate", os.path.join(icons_dir, "separate.png"), 'IMAGE')
+        _icon_collection.load("icon_cube", os.path.join(icons_dir, "cube.png"), 'IMAGE')
+        _icon_collection.load("icon_shaderball", os.path.join(icons_dir, "shaderBall.png"), 'IMAGE')
+        _icon_collection.load("icon_knife", os.path.join(icons_dir, "knife.png"), 'IMAGE')
+        _icon_collection.load("icon_subdcube", os.path.join(icons_dir, "subdCube.png"), 'IMAGE')
+        _icon_collection.load("icon_brush", os.path.join(icons_dir, "brush.png"), 'IMAGE')
+        _icon_collection.load("icon_mask", os.path.join(icons_dir, "mask.png"), 'IMAGE')
+        _icon_collection.load("icon_grid", os.path.join(icons_dir, "grid.png"), 'IMAGE')
+        _icon_collection.load("icon_extract", os.path.join(icons_dir, "extract.png"), 'IMAGE')
+        _icon_collection.load("icon_delete", os.path.join(icons_dir, "delete.png"), 'IMAGE')
+        _icon_collection.load("icon_undo", os.path.join(icons_dir, "undo.png"), 'IMAGE')
+        _icon_collection.load("icon_switcharrow", os.path.join(icons_dir, "switchArrow.png"), 'IMAGE')
     
-
-    # modals
-    custom_icons.load("icon_separate", os.path.join(icons_dir, "separate.png"), 'IMAGE')
-    custom_icons.load("icon_cube", os.path.join(icons_dir, "cube.png"), 'IMAGE')
-    custom_icons.load("icon_shaderball", os.path.join(icons_dir, "shaderBall.png"), 'IMAGE')
-    custom_icons.load("icon_knife", os.path.join(icons_dir, "knife.png"), 'IMAGE')
-    custom_icons.load("icon_subdcube", os.path.join(icons_dir, "subdCube.png"), 'IMAGE')
-    custom_icons.load("icon_brush", os.path.join(icons_dir, "brush.png"), 'IMAGE')
-    custom_icons.load("icon_mask", os.path.join(icons_dir, "mask.png"), 'IMAGE')
-    custom_icons.load("icon_grid", os.path.join(icons_dir, "grid.png"), 'IMAGE')
-    custom_icons.load("icon_extract", os.path.join(icons_dir, "extract.png"), 'IMAGE')
-    custom_icons.load("icon_delete", os.path.join(icons_dir, "delete.png"), 'IMAGE')
-    custom_icons.load("icon_undo", os.path.join(icons_dir, "undo.png"), 'IMAGE')
-    custom_icons.load("icon_switcharrow", os.path.join(icons_dir, "switchArrow.png"), 'IMAGE')
-   
-
-
-    dh_tools_icon_collections["main"] = custom_icons
-    dh_tools_icons_loaded = True
-
-    return dh_tools_icon_collections["main"]
-
+    return _icon_collection
 
 def dh_tools_clear_icons():
-    global dh_tools_icons_loaded
-    for icon in dh_tools_icon_collections.values():
-        bpy.utils.previews.remove(icon)
-    dh_tools_icon_collections.clear()
-    dh_tools_icons_loaded = False
+    global _icon_collection
+    if _icon_collection:
+        bpy.utils.previews.remove(_icon_collection)
+        _icon_collection = None
